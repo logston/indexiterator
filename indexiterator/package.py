e062c7055d9c6fccc22d635de823b1dc415862db
Paul Logston <code@logston.me>
2016-01-19 01:02:42 -0500

    Add Release model

--- indexiterator/package.py
+++ indexiterator/package.py
@@ -1,4 +1,5 @@
 from collections import OrderedDict
+from datetime import datetime
 import json
 from urllib.parse import urljoin
 
@@ -7,11 +8,30 @@ import requests
 from indexiterator import constants
 
 
+def min_upload_time(key, releases):
+    release_list = releases.get(key)
+    min_ts = None
+    for release in release_list:
+        upload_dt_str = release.get('upload_time')
+        dt_format = '%Y-%m-%dT%H:%M:%S'
+
+        try:
+            upload_dt = datetime.strptime(upload_dt_str, dt_format)
+        except ValueError:
+            continue
+
+        if min_ts is None or upload_dt < min_ts:
+            min_ts = upload_dt
+
+    return min_ts
+
+
 class PackageDetailsMixin:
     def _get_releases(self):
         releases = self.data.get('releases', {})
         ordered_releases = OrderedDict()
-        keys = sorted(releases.keys())
+        keys = (key for key, value in releases.items() if value)
+        keys = sorted(keys, key=lambda key: min_upload_time(key, releases))
         for key in keys:
             ordered_releases[key] = releases[key]
         return ordered_releases
@@ -70,4 +90,31 @@ class Package(PackageDetailsMixin):
     def _get_json_data(self):
         response = requests.get(self.json_url)
         return response.content.decode()
+
+    def __iter__(self):
+        return (Release(version, self) for version in self.releases)
+
+
+class Release:
+
+    def __init__(self, version, package):
+        self.version = version
+        self.package = package
+
+    def get_tarball(self):
+        release_list = self.package.releases.get(self.version)
+        for release in release_list:
+            if release.get('url', '').endswith('.tar.gz'):
+                break
+
+        if not release:
+            return
+
+        url = release.get('url')
+        response = requests.get(url)
+
+        return response.content
+
+    def __repr__(self):
+        return "<Release '{}:{}'>".format(self.package.name, self.version)
 
